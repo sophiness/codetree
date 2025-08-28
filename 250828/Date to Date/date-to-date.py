11b92ddf9e949925e527eade06e070530de759fe
sophiness <12sophie19@gmail.com>
2025-08-28 21:39:40 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250828/Date to Date/date-to-date.py
+++ 250828/Date to Date/date-to-date.py
@@ -6,12 +6,12 @@ days = [31,28,31,30,31,30,31,31,30,31,30,31]
 
 md1 = 0
 md2 = 0
-for i in range(m2):
+for i in range(m2-1):
     md2 += days[i]
-for j in range(m1):
+for j in range(m1-1):
     md1 += days[j]
 
 md2 += d2
 md1 += d1
 
-print(md2-md1)
+print(md2-md1+1)
