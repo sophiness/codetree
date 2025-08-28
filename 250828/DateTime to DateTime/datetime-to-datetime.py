ca1baa3cd640d6af220e143e42be75514eaa0a56
sophiness <12sophie19@gmail.com>
2025-08-28 21:49:20 +0900

    Title: DateTime to DateTime, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250828/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,11 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+if a>=11 and b>=11 and c>11:
+    a-=11
+    b-=11
+    c-=11
+    print(a*24*60 + b*60 + c)
+
+else:
+    print(-1)
