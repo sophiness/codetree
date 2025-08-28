ec714e96641fafda36f7b84efb8f68b388469e59
sophiness <12sophie19@gmail.com>
2025-08-28 21:36:13 +0900

    Title: Date to Date, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250828/Date to Date/date-to-date.py
@@ -0,0 +1,17 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+
+days = [31,28,31,30,31,30,31,31,30,31,30,31]
+
+md1 = 0
+md2 = 0
+for i in range(m2):
+    md2 += days[i]
+for j in range(m1):
+    md1 += days[j]
+
+md2 += d2
+md1 += d1
+
+print(md2-md1)
