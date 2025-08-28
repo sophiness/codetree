743f14439d2d94de9323af2cfe8ee54daf16b8aa
sophiness <12sophie19@gmail.com>
2025-08-28 22:00:07 +0900

    Title: DateTime to DateTime, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 250828/DateTime to DateTime/datetime-to-datetime.py
+++ 250828/DateTime to DateTime/datetime-to-datetime.py
@@ -1,11 +1,12 @@
 a, b, c = map(int, input().split())
 
 # Please write your code here.
-if a>=11 and b>=11 and c>=11:
-    a-=11
-    b-=11
-    c-=11
-    print(a*24*60 + b*60 + c)
 
+dt2 = a*24*60 + b*60 + c
+dt1 = 11*24*60 + 11*60 + 11
+
+ans = dt2 - dt1
+if ans>=0:
+    print(ans)
 else:
     print(-1)
