ec429dbe8d5e261b942849f0ebd7d337b1af8dd1
sophiness <12sophie19@gmail.com>
2025-08-29 11:35:00 +0900

    Title: 그 요일은, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250829/그 요일은/the-day-of-the-day.py
+++ 250829/그 요일은/the-day-of-the-day.py
@@ -10,8 +10,8 @@ A = int(A)
 def num_of_days(m,d):
     days = [0,31,29,31,30,31,30,31,31,30,31,30,31]
     for i in range(1,m):
-        m+=days[i]
-    return m+d
+        d+=days[i]
+    return d
 
 diff = num_of_days(m2,d2)-num_of_days(m1,d1)+1
 if diff%7>A:
