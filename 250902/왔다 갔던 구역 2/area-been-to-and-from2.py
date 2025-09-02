6f83fbb63ca61dc5e844d7a6764cfafe59632ed2
sophiness <12sophie19@gmail.com>
2025-09-02 12:41:32 +0900

    Title: 왔다 갔던 구역 2, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250902/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,30 @@
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+# Please write your code here.
+block = [0]*200
+idx = 99
+
+for i in range(n):
+    
+    if dir[i] == 'R':
+        for j in range(x[i]):
+            block[idx] += 1
+            idx += 1
+    elif dir[i] == 'L':
+        for k in range(x[i]):
+            idx -= 1
+            block[idx] += 1
+ans = 0
+for b in block:
+    if b>=2:
+        ans+=1
+
+print(ans)
+
+    
